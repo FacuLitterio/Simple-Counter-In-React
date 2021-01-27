fe28b2b5a15152fd2b9c590c988c2b5dc398d3a9
Facundo Litterio <facundolitterio@yahoo.com>
2021-01-27 19:36:52 -0300

    Unique commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = `Has clickeado ${contador}`;
+  });
+
+  const [contador, setContador] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Display">
+        <h3>Cuenta: {contador}</h3>
+        <button type="button" onClick={() => setContador(contador - 1)}>
+          Restar
+        </button>
+        <button type="button" onClick={() => setContador(contador + 1)}>
+          Sumar
+        </button>
+      </div>
     </div>
   );
 }
